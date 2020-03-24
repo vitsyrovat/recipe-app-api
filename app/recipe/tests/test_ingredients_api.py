df7dc375f597931547c6c84df00bbd275c415253
vitek <vitsyrovat@gmail.com>
2020-03-24 14:25:43 +0100

    Added recipe detail view.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -44,7 +44,7 @@ class PrivateIngredientsApiTests(TestCase):
 
         res = self.client.get(INGREDIENTS_URL)
 
-        ingredients = Ingredient.objects.all().order_by('-name')
+        ingredients = Ingredient.objects.all().order_by('name')
         serializer = IngredientSerializer(ingredients, many=True)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,7 +15,7 @@ class BaseRecipeAttrViewSet(GenericViewSet, ListModelMixin, CreateModelMixin):
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return self.queryset.filter(user=self.request.user).order_by('name')
 
     def perform_create(self, serializer):
         """Create a new object"""
@@ -44,3 +44,10 @@ class RecipeViewSet(ModelViewSet):
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
